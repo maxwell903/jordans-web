f34ffe7d3456a4c3b651b80b4d2bb867d5e43dc8
maxwell903 <maxwayne903@gmail.com>
2024-12-15 13:44:42 -0500

    cmon

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@
 const nextConfig = {
   images: {
     unoptimized: true,
-  }
+  },
+  output: 'export',  // Add this line back
+  distDir: 'out'     // Change this to 'out'
 }
 
 module.exports = nextConfig
