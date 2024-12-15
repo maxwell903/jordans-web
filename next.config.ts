56ffd404bcecd92b4cb6ee13c4dea07bfe9ded49
maxwell903 <maxwayne903@gmail.com>
2024-12-15 13:10:17 -0500

    netlify

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',  // Add this line
   images: {
     unoptimized: true,
   },
-  output: 'standalone',
 }
 
 module.exports = nextConfig
